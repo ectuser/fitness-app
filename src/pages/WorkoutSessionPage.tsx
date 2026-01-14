052ff0b488a5c792da9fdb3db0ea46f7168b6988
Ivan Menshchikov <ivan@scrypt.swiss>
2026-01-14 01:02:52 +0100

    refactor: remove unused Badge import from WorkoutSessionPage to streamline component code

--- src/pages/WorkoutSessionPage.tsx
+++ src/pages/WorkoutSessionPage.tsx
@@ -3,7 +3,6 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { useData } from '@/context/DataContext';
 import { PageHeader } from '@/components/layout/PageHeader';
 import { Button } from '@/components/ui/button';
-import { Badge } from '@/components/ui/badge';
 import {
   AlertDialog,
   AlertDialogAction,
